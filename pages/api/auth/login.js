7ac3bfa23369b6b00df9579396437274949bcb63
Baiaman <taalaibekov003@gmail.com>
2021-06-17 17:32:15 +0600

    msg password incorect

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -18,10 +18,10 @@ const login = async (req, res) => {
     const { email, password } = req.body
 
     const user = await Users.findOne({ email })
-    if (!user) return res.status(400).json({ err: 'This user does not exist.' })
+    if (!user) return res.status(400).json({ err: 'Такого пользователя не существует.' })
 
     const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) return res.status(400).json({ err: 'Incorrect password.' })
+    if (!isMatch) return res.status(400).json({ err: 'Не правильный пороль.' })
 
     const access_token = createAccessToken({ id: user._id })
     const refresh_token = createRefreshToken({ id: user._id })
